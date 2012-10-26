e9d639643c022ddce9a166f4c78264562c7f148e
csteipp <csteipp@wikimedia.org>
2012-10-26 11:51:27 -0700

    Initial Commit of Wikivoyage "BreadCrumbs"
    Change-Id: Iff1966b5f863bf13b7da4a4889b5ec45879c2005

---
+++ BreadCrumbs.i18n.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Internationalisation file for BreadCrumbs extension.
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'bct-delimiter'  => '&gt;',
+	'bct-desc'       => 'The extension enables the output af a bread-crumbs navigation',
+);
+
+$messages['de'] = array(
+	'bct-delimiter'  => '&gt;',
+	'bct-desc'       => 'Die Erweiterung ermöglicht die Ausgabe eine Brotkrümelnavigation',
+);
+
+?>
